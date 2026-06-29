35ffe0a6ac1ef40a22756a4dceebe57c2df54862
Abhishek Anand <anandabhishek.dev@gmail.com>
2026-06-29 22:26:00 +0530

    Day_01 Python introduction

---
+++ Day_01/introduction_to_python.py
@@ -0,0 +1,12 @@
+# ==========================================
+# Day 01 - Introduction to Python
+# ==========================================
+
+# Printing a simple message
+print("Hello, World!")
+
+# Practice
+print("Hello Colab! How are you today?")
+
+# Finding the length of a string
+print(len("Hello Colab! How are you today?"))
